8f2162c70d0274f7c784f67a1c5d88bd690b7d45
zachm17 <zachmoy17@gmail.com>
2015-11-09 09:47:46 -0700

    made the GUI actually visible

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,12 +3,13 @@ package gui.view;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.SpringLayout; //For layout
 import gui.controller.GUIController;
 
 /**
  * 
  * @author Zach Moyes
- *
+ *version 0.1 Nov 9, 2015
  */
 
 public class GUIPanel extends JPanel
@@ -16,11 +17,13 @@ public class GUIPanel extends JPanel
 	private GUIController baseController;
 	private JButton firstButton;
 	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
 		
+		baseLayout = new SpringLayout();
 		firstButton = new JButton("Please do not click the button Greyson");
 		firstTextField = new JTextField("words can be typed here bruv");
 		
@@ -28,10 +31,14 @@ public class GUIPanel extends JPanel
 		setupLayout();
 		setupListeners();
 	}
-	
+	/**
+	 * Helper method to load all GUI Components into the panel
+	 */
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
 	}
 	
 	private void setupLayout()
